a9fced273e5feebbd734e5d250a0f1f83b652212
pedrovt <pedro.teix@ua.pt>
2018-12-06 00:21:33 +0000

    Added Android App Source Code
    Source for the base code: https://github.com/slymax/webview/archive/master.zip

---
+++ android-app/src/app/src/main/java/com/ams/cityrunning/MainActivity.java
@@ -0,0 +1,46 @@
+package com.ams.cityrunning;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+
+        // Webview
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // REMOTE RESOURCE
+        mWebView.loadUrl("http://cityrunning.westeurope.cloudapp.azure.com:8080");
+        //mWebView.setWebViewClient(new MyWebViewClient());
+
+    }
+
+    // Prevent the back-button from closing the app
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
